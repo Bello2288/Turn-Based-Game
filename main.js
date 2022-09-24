e08927b19e867b1c4b67d695688bcbba9ac21512
Adam Brower-Lingsch <adambrower-lingsch@Adams-MacBook-Air.local>
2022-09-24 15:41:31 -0400

    adds enemy attack

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const playerAttackStrong = document.querySelector(".knightButton1");
 const playerAttackMed = document.querySelector(".knightButton2");
 const playerAttackWeak = document.querySelector(".knightButton3");
 const enemyCurrentHealth = document.querySelector(".enemy-health");
+const playerCurrentHealth = document.querySelector(".player-health");
+let delayInMilliseconds = 1000;
 
 const rng = function randomNumber(min, max) { 
     return Math.round(Math.random() * (max - min) + min);
@@ -15,17 +17,28 @@ function strongAttack(button) {
 
     enemyCurrentHealth.innerHTML = parseInt(enemyCurrentHealth.innerHTML) - rng(0, 20);
        
+    setTimeout(function(){
+        playerCurrentHealth.innerHTML = parseInt(playerCurrentHealth.innerHTML) - rng(0, 20);
+    }, delayInMilliseconds);
 }
 
 function medAttack(button) {
 
     enemyCurrentHealth.innerHTML = parseInt(enemyCurrentHealth.innerHTML) - rng(0, 10);
+
+    setTimeout(function(){
+        playerCurrentHealth.innerHTML = parseInt(playerCurrentHealth.innerHTML) - rng(0, 10);
+    }, delayInMilliseconds);
            
  }
     
  function weakAttack(button) {
 
-    enemyCurrentHealth.innerHTML = parseInt(enemyCurrentHealth.innerHTML) - (rng(0, 20)-1);
+    enemyCurrentHealth.innerHTML = parseInt(enemyCurrentHealth.innerHTML) - (rng(0, 5)-1);
+
+    setTimeout(function(){
+        playerCurrentHealth.innerHTML = parseInt(playerCurrentHealth.innerHTML) - (rng(0, 5)-1);
+    }, delayInMilliseconds);
        
  }
 
